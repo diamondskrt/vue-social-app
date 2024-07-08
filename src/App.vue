e1ebb070b1986db3ee541b1b5e100b220a936245
diamondskrt <create7321@gmail.com>
2024-07-08 12:43:18 +0300

    chore: init app

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-4">
+    <h1 class="text-3xl font-bold underline mb-2">Hello world!</h1>
+    <el-button type="primary">Primary</el-button>
+  </div>
+</template>
